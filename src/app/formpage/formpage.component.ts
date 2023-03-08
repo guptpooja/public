9a69963157032957ae11bb180620fde14418ddb1
guptpooja <guptapooja5554@gmail.com>
2023-03-08 14:46:32 +0530

    first commit

--- src/app/formpage/formpage.component.ts
+++ src/app/formpage/formpage.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 export const formObj = [{
+  id:'',
   firstname: '',
   middlename: '',
   lastname: '',
@@ -21,8 +22,9 @@ export const formObj = [{
   styleUrls: ['./formpage.component.css']
 })
 export class FormpageComponent implements OnInit {
-  formArr: any[] = [];
+  formArr: any = [];
   formObj: any = {
+    id:'',
     firstname: '',
     middlename: '',
     lastname: '',
@@ -34,16 +36,24 @@ export class FormpageComponent implements OnInit {
     add1: '',
     add2: ''
   };
-  session: any;
+
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   constructor(private router: Router) { }
 
   SaveData(form: NgForm) {
-    this.router.navigate(['/viewpage'])
-    this.formArr.push(this.formObj);
+    console.log(formObj);
+    console.log(this.formObj);
+    if (localStorage.getItem('session') != null) {
+      let tempSession: any = localStorage.getItem('session');
+      this.formArr = JSON.parse(tempSession)
+      this.formArr.push(this.formObj);
+    } else {
+      this.formArr.push(this.formObj);
+    }
     localStorage.setItem('session', JSON.stringify(this.formArr));
+    this.router.navigate(['/viewpage'])
   }
 
   ngOnInit(): void {
--- src/app/viewpage/viewpage.component.ts
+++ src/app/viewpage/viewpage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-viewpage',
   templateUrl: './viewpage.component.html',
@@ -12,22 +11,33 @@ export class ViewpageComponent implements OnInit {
     'middlename': '',
     'lastname': ''
   }];
-
-  displayedColumns: string[] = ['firstname', 'middlename', 'lastname'];
-  dataSource = this.viewform;
-
+  viewData:any = [];
+  displayedColumns: string[] = ['firstname', 'middlename', 'lastname','Action'];
+  dataSource:any = [];
+  // dataSource = new ExampleDataSource(this.viewform);
   constructor() { }
 
-
-
- 
-
   ngOnInit(): void {
-    let viewData: any = localStorage.getItem('session');
-    if(viewData != null){
-    this.viewform = JSON.parse(viewData);
+    this.viewData = localStorage.getItem('session') || null;
+    if(this.viewData != null){
+    this.viewData = JSON.parse(this.viewData);
     }
-    console.log('some data ', this.viewform);
+    console.log('some data ', this.viewData)
+  this.dataSource = this.viewData;
+
   }
 
+  deleteData(index:any){
+    this.viewData.splice(0,index+1);
+    console.log(this.viewData);
+    // this.dataSource = [...this.viewData];
+    this.dataSource.next(this.viewData);
+    // this.dataSource.refresh()
+    // localStorage.setItem('session', JSON.stringify(this.viewData));
+    // console.log(index);
+    // this.viewData = localStorage.getItem('session');
+    // this.dataSource = this.viewData;
+  //  this.dataSource.setData(this.viewData);
+    // this.dataSource = this.dataSource.data.filter((i:any) => i !== index)
+  }
 }
